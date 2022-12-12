0d363cd24a01df0163a2d4f5b72172800fe4e4c0
Mahmoud elali <Mahmoudelali@github.com>
2022-12-12 23:13:38 +0200

    Completed JOINS

--- answers.sh
+++ answers.sh
@@ -35,3 +35,13 @@ UPDATE graduates SET Graduation = '08/09/2018' WHERE name = 'Layal';
 # 12
 DELETE FROM students WHERE name = 'Layal' ;
 
+# 14
+SELECT employees.Name , employees.Company , companies.Date FROM employees JOIN companies on companies.ID = employees.ID ; 
+
+# 15
+SELECT employees.name FROM employees JOIN companies on companies.id	= employees.ID  WHERE Date < 2000;
+
+# 16
+SELECT employees.name , employees.Role   FROM employees JOIN companies on companies.id	= employees.ID WHERE Role = 'Graphic Designer' ;
+
+
